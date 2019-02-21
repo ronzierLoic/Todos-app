a4164401483bf544b5b83442f02464d4d3efca5a
lpiem <loic.ronzier@etu.univ-lyon1.fr>
2019-02-21 11:45:01 +0100

    [FEAT] add save

--- todos-app/presentation/ui/viewcontroller/CheckListViewController.swift
+++ todos-app/presentation/ui/viewcontroller/CheckListViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 
 class CheckListViewController: UITableViewController, ItemViewControllerDelegate {
     var checkItemList: [CheckListItem] = []
+    static var documentDirectory: URL = (FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first)!
+    static var dataFileUrl: URL = documentDirectory.appendingPathComponent("Checklist").appendingPathExtension("json")
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.checkItemList.append(CheckListItem(text: "Je suis check", checked: true))
-        self.checkItemList.append(CheckListItem(text: "Je suis pas check"))
+        
+    }
+    
+    override func awakeFromNib() {
+        loadCheckListItems()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -22,9 +27,9 @@ class CheckListViewController: UITableViewController, ItemViewControllerDelegate
             let addItemViewController = (segue.destination as! UINavigationController).topViewController as! ItemViewController
             addItemViewController.delegate = self
         } else if (segue.identifier == "editItem") {
-           
+            
             if let cell = sender as? UITableViewCell,
-            let indexPath = tableView.indexPath(for: cell){
+                let indexPath = tableView.indexPath(for: cell){
                 let addItemViewController = (segue.destination as! UINavigationController).topViewController as! ItemViewController
                 addItemViewController.itemToEdit = checkItemList[indexPath.row]
                 addItemViewController.delegate = self
@@ -36,6 +41,7 @@ class CheckListViewController: UITableViewController, ItemViewControllerDelegate
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.checkItemList.count
     }
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CheckListItem", for: indexPath) as! CheckMarkViewCell
@@ -55,6 +61,7 @@ class CheckListViewController: UITableViewController, ItemViewControllerDelegate
         if editingStyle == .delete {
             self.checkItemList.remove(at: indexPath.row)
             self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            saveCheckListItems()
         }
     }
     
@@ -66,6 +73,7 @@ class CheckListViewController: UITableViewController, ItemViewControllerDelegate
         self.dismiss(animated: false, completion: nil)
         self.checkItemList.append(item)
         tableView.insertRows(at: [IndexPath(row: self.checkItemList.count-1, section: 0)], with: .none)
+        saveCheckListItems()
     }
     
     func itemViewController(_ controller: ItemViewController, didFinishEditingItem item: CheckListItem) {
@@ -73,6 +81,7 @@ class CheckListViewController: UITableViewController, ItemViewControllerDelegate
         if let row = self.checkItemList.firstIndex(where: {$0 === item}) {
             self.checkItemList[row] = item
             tableView.reloadRows(at: [IndexPath(row: row, section:0)], with: .none)
+            saveCheckListItems()
         }
     }
     
@@ -86,5 +95,19 @@ extension CheckListViewController {
     func configureText(for cell: CheckMarkViewCell, withItem item: CheckListItem) {
         cell.nameLabel.text = item.text
     }
+    
+    func saveCheckListItems() {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        let data = try? encoder.encode(checkItemList)
+        try? data?.write(to: CheckListViewController.dataFileUrl)
+    }
+    
+    func loadCheckListItems() {
+        if let data = try? Data(contentsOf: CheckListViewController.dataFileUrl),
+            let list = try? JSONDecoder().decode([CheckListItem].self, from: data) {
+            self.checkItemList = list
+        }
+    }
+        
 }
-
